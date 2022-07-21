f0e4da1cf90dc04244e0901bcc64b98aa80a281c
Gabriel Lobo <globo.coding@gmail.com>
2022-07-20 20:30:52 -0600

    Ejercicio Bombilla

---
+++ Semana 7/Ejercicios/Ejercicio Bombilla/js/bombilla.js
@@ -0,0 +1,14 @@
+let buttonBulbOff = document.getElementById("turnOff");
+let buttonBulbOn = document.getElementById("turnOn");
+let bulbImage = document.getElementById("bulbImg");
+
+const turnBulbOn = () => {
+  bulbImage.src = "./assets/pic_bulbon.gif";
+};
+
+const turnBulbOff = () => {
+  bulbImage.src = "./assets/pic_bulboff.gif";
+};
+
+buttonBulbOff.addEventListener("click", turnBulbOff);
+buttonBulbOn.addEventListener("click", turnBulbOn);
